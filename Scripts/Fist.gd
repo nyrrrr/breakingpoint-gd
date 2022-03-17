d379848ee3ed4f38080aed3fad04596c939f4e2b
Thilo Mothes <tmothes@gmail.com>
2022-03-17 20:04:25 +0100

    fist now returns on hit or button release

--- Scripts/Fist.gd
+++ Scripts/Fist.gd
@@ -6,6 +6,7 @@ export var speed = 600
 var screen_size
 var starting_position: Vector2
 var velocity: Vector2
+var is_returning: bool
 
 var damage = 1
 
@@ -17,18 +18,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity = Vector2.ZERO
-	if Input.is_action_pressed("shoot_left"):
+	if Input.is_action_pressed("shoot_left") and is_returning == false:
 		velocity.x -= 1
-	if Input.is_action_pressed("shoot_right"):
+	if Input.is_action_pressed("shoot_right") and is_returning == false:
 		velocity.x += 1
-	if Input.is_action_pressed("shoot_up"):
+	if Input.is_action_pressed("shoot_up") and is_returning == false:
 		velocity.y -= 1
-	if Input.is_action_pressed("shoot_down"):
+	if Input.is_action_pressed("shoot_down") and is_returning == false:
 		velocity.y += 1
-	if !Input.is_action_pressed("shoot_left") && !Input.is_action_pressed("shoot_right") && !Input.is_action_pressed("shoot_up") && !Input.is_action_pressed("shoot_down"):
+	if is_returning or (!Input.is_action_pressed("shoot_left") && !Input.is_action_pressed("shoot_right") && !Input.is_action_pressed("shoot_up") && !Input.is_action_pressed("shoot_down")):
+		is_returning = true
 		velocity = starting_position - self.position
-		if velocity.length() < 10:
+		if velocity.length() < 5:
 			velocity = Vector2.ZERO
+			self.position = starting_position
+			is_returning = false
 	velocity = velocity.normalized() * speed
 	self.global_position += velocity * delta
 	self.global_position.x = clamp(self.global_position.x, 0, screen_size.x)
@@ -37,4 +41,5 @@ func _process(delta):
 func _on_Fist_body_entered(body):
 	if body is Enemy:
 		body.health -= damage
+		is_returning = true
 		# bounce the enemy back
